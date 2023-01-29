d5134f99df10cfbd5b83b2711655d8c4e7eee2ff
rohmatsetiawanmta <rohmatsetiawanmta@gmail.com>
2023-01-29 15:07:14 +0700

    Check

--- pos-app/src/App.js
+++ pos-app/src/App.js
@@ -2,6 +2,10 @@
 import { Routes, Route } from "react-router-dom";
 //Import pages
 import Cashier from "./pages/cashier";
+import SalesReports from "./pages/salesReports";
+import UserManagement from "./pages/userManagement";
+import Payment from "./pages/payment";
+import AdminDashboard from "./pages/adminDashboard";
 import Login from "./pages/login";
 //Import components
 import Sidebar from "./components/sidebar";
@@ -13,6 +17,10 @@ function App() {
       <Sidebar />
       <Routes>
         <Route path="/" element={<Cashier />} />
+        <Route path="/sales-reports" element={<SalesReports />} />
+        <Route path="/user-management" element={<UserManagement />} />
+        <Route path="/payment" element={<Payment />} />
+        <Route path="/admin-dashboard" element={<AdminDashboard />} />
         <Route path="/login" element={<Login />} />
       </Routes>
     </div>
