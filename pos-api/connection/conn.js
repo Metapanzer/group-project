725f94359834eed1eea458adc94fee0f9701e50d
Krisna Sandy Pribadi <baltheon@gmail.com>
2023-01-29 10:34:21 +0700

    add conn.js

---
+++ pos-api/connection/conn.js
@@ -0,0 +1,18 @@
+//Import mysql dependencies
+const mysql = require("mysql2");
+
+//Database connection setting
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "1234",
+  database: "pos-app",
+});
+
+db.connect((err) => {
+  if (err) return console.log("Error" + err.message);
+
+  console.log("Connected to Database");
+});
+
+module.exports = db;
