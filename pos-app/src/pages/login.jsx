44698a76b75e7207fd06f43b1801c41961ac300a
rohmatsetiawanmta <rohmatsetiawanmta@gmail.com>
2023-01-29 16:13:08 +0700

    Finish Login Validation and Hot Toast

--- pos-app/src/pages/login.jsx
+++ pos-app/src/pages/login.jsx
@@ -1,6 +1,8 @@
 import axios from "axios";
 
 import { useRef, useState } from "react";
+import toast, { Toaster } from 'react-hot-toast';
+
 
 export default function Login() {
   const [message, setMessage] = useState("");
@@ -8,19 +10,24 @@ export default function Login() {
   let username = useRef();
   let password = useRef();
 
-  //   let onLogin = async () => {
-  //     try {
-  //       let response = await axios.get(
-  //         `http://localhost:5000/users?username=${username.current.value}&password=${password.current.value}`
-  //       );
+  let onLogin = async () => {
+    try {
+      let inputUsername = username.current.value;
+      let inputPassword = password.current.value;
+      if (!inputUsername || !inputPassword) throw {message: 'Complete your data!'}
 
-  //       if (response.data.length === 0) throw { message: "Account Not Found" };
-  //       alert("Login Success");
-  //       setMessage("");
-  //     } catch (error) {
-  //       setMessage(error.message);
-  //     }
-  //   };
+      let response = await axios.get(
+        `http://localhost:5000/users?username=${username.current.value}&password=${password.current.value}`
+      );
+
+      if (response.data.length === 0) throw { message: "Account Not Found" };
+      toast.success("Login Success");
+      setMessage("");
+    } catch (error) {
+      setMessage(error.message);
+      toast.error(error.message)
+    }
+  };
 
   return (
     <div className="bg-[#252836] text-white h-[100vh] w-[100vw] flex items-center justify-center">
@@ -40,14 +47,15 @@ export default function Login() {
               ref={password}
               type="password"
               placeholder="Password"
-              className="py-2 px-2 w-[30vw] h-[8vh] m-1 mb-4 rounded-md text-[#1F1D2B]"
+              className="py-2 px-2 w-[30vw] h-[8vh] m-1 rounded-md text-[#1F1D2B]"
             />
           </div>
           <div>
-            <a className="text-md	text-center px-3 py-2  align border border-[#252836] ml-1 bg-[#252836] text-white rounded-lg hover:cursor-pointer hover:bg-white hover:text-[#252836] hover:duration-150">
+            <button onClick={onLogin} className="text-mdtext-center px-3 py-2 align border border-[#252836] ml-1 bg-[#252836] text-white rounded-lg hover:cursor-pointer hover:bg-white hover:text-[#252836] hover:duration-150">
               Login
-            </a>
+            </button>
           </div>
+          <div><Toaster/></div>
         </div>
       </div>
     </div>
