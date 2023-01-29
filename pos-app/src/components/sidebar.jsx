44a8ebc77075c4bfd9494e20c8c72340bd26502f
Krisna Sandy Pribadi <baltheon@gmail.com>
2023-01-29 14:46:13 +0700

    add link to sidebar

--- pos-app/src/components/sidebar.jsx
+++ pos-app/src/components/sidebar.jsx
@@ -1,3 +1,5 @@
+//Import dependencies
+import { Link } from "react-router-dom";
 //Import logo
 import restaurant from "../assets/store.png";
 import { BiHomeAlt } from "react-icons/bi";
@@ -9,28 +11,40 @@ import { BiLogIn } from "react-icons/bi";
 
 export default function Sidebar() {
   return (
-    <div className="sticky left-0 flex flex-col gap-5 p-6 h-full w-24 rounded-r-xl bg-black justify-start items-center">
-      <div className="bg-[#EB966A]/25 drop-shadow-md rounded-md w-14 h-14 flex justify-center items-center">
-        <img src={restaurant} alt="restaurant" className="h-12 w-12" />
+    <div className="sticky left-0 flex flex-col gap-6 p-6 h-full w-24 rounded-r-xl bg-black justify-start items-center">
+      <div className="bg-[#EB966A]/25 drop-shadow-md rounded-lg w-14 h-14 flex justify-center items-center">
+        <img src={restaurant} alt="restaurant" className="h-11 w-11" />
       </div>
-      <div className="group hover:bg-[#EA7C69] drop-shadow-md rounded-md w-14 h-14 flex justify-center items-center">
-        <BiHomeAlt className="h-6 w-6 text-[#EA7C69] group-hover:text-white" />
-      </div>
-      <div className="group hover:bg-[#EA7C69] drop-shadow-md rounded-md w-14 h-14 flex justify-center items-center">
-        <AiOutlinePieChart className="h-6 w-6 text-[#EA7C69] group-hover:text-white" />
-      </div>
-      <div className="group hover:bg-[#EA7C69] drop-shadow-md rounded-md w-14 h-14 flex justify-center items-center">
-        <FiUsers className="h-6 w-6 text-[#EA7C69] group-hover:text-white" />
-      </div>
-      <div className="group hover:bg-[#EA7C69] drop-shadow-md rounded-md w-14 h-14 flex justify-center items-center">
-        <BsCart3 className="h-6 w-6 text-[#EA7C69] group-hover:text-white" />
-      </div>
-      <div className="group hover:bg-[#EA7C69] drop-shadow-md rounded-md w-14 h-14 flex justify-center items-center">
-        <MdOutlineAdminPanelSettings className="h-6 w-6 text-[#EA7C69] group-hover:text-white" />
-      </div>
-      <div className="group hover:bg-[#EA7C69] drop-shadow-md rounded-md w-14 h-14 flex justify-center items-center">
-        <BiLogIn className="h-6 w-6 text-[#EA7C69] group-hover:text-white" />
-      </div>
+      <Link to="/">
+        <div className="group hover:bg-[#EA7C69] drop-shadow-md rounded-md w-14 h-14 flex justify-center items-center">
+          <BiHomeAlt className="h-6 w-6 text-[#EA7C69] group-hover:text-white" />
+        </div>
+      </Link>
+      <Link to="/sales-reports">
+        <div className="group hover:bg-[#EA7C69] drop-shadow-md rounded-md w-14 h-14 flex justify-center items-center">
+          <AiOutlinePieChart className="h-6 w-6 text-[#EA7C69] group-hover:text-white" />
+        </div>
+      </Link>
+      <Link to="/user-management">
+        <div className="group hover:bg-[#EA7C69] drop-shadow-md rounded-md w-14 h-14 flex justify-center items-center">
+          <FiUsers className="h-6 w-6 text-[#EA7C69] group-hover:text-white" />
+        </div>
+      </Link>
+      <Link to="/payment">
+        <div className="group hover:bg-[#EA7C69] drop-shadow-md rounded-md w-14 h-14 flex justify-center items-center">
+          <BsCart3 className="h-6 w-6 text-[#EA7C69] group-hover:text-white" />
+        </div>
+      </Link>
+      <Link to="/admin-dashboard">
+        <div className="group hover:bg-[#EA7C69] drop-shadow-md rounded-md w-14 h-14 flex justify-center items-center">
+          <MdOutlineAdminPanelSettings className="h-6 w-6 text-[#EA7C69] group-hover:text-white" />
+        </div>
+      </Link>
+      <Link to="/login">
+        <div className="group hover:bg-[#EA7C69] drop-shadow-md rounded-md w-14 h-14 flex justify-center items-center">
+          <BiLogIn className="h-6 w-6 text-[#EA7C69] group-hover:text-white" />
+        </div>
+      </Link>
     </div>
   );
 }
