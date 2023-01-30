196eb7bf18ed018025f09de2fcc07115c20826be
Krisna Sandy Pribadi <baltheon@gmail.com>
2023-01-30 18:02:37 +0700

    create controllers and routers

--- pos-app/src/components/card.jsx
+++ pos-app/src/components/card.jsx
@@ -1,3 +1,4 @@
+//Import components
 import { Button } from "@chakra-ui/react";
 import { BsCart3 } from "react-icons/bs";
 
@@ -5,7 +6,7 @@ export default function Card() {
   return (
     <div className="bg-[#1F1D2B] p-2 w-48 h-56 rounded-lg text-white flex flex-col items-center gap-1">
       <img
-        src="/image/gramercy-tavern-burger.jpg"
+        src="/image/burgers/gramercy-tavern-burger.jpg"
         alt="gramercy-tavern-burger"
         className="w-28 h-28 rounded-full"
       />
