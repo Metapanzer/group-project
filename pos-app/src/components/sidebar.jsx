dc4dc39611a63aa1cd2dfbf4fa39add3baceeb2c
Krisna Sandy Pribadi <baltheon@gmail.com>
2023-01-30 16:16:33 +0700

    edit sidebar icon position

--- pos-app/src/components/sidebar.jsx
+++ pos-app/src/components/sidebar.jsx
@@ -11,40 +11,44 @@ import { BiLogIn } from "react-icons/bi";
 
 export default function Sidebar() {
   return (
-    <div className="sticky left-0 flex flex-col gap-6 p-6 h-full w-24 bg-black justify-start items-center">
-      <div className="bg-[#EB966A]/25 drop-shadow-md rounded-lg w-14 h-14 flex justify-center items-center">
-        <img src={restaurant} alt="restaurant" className="h-11 w-11" />
+    <div className="sticky left-0 flex flex-col gap-6 p-5 h-full w-24 bg-black justify-between items-center">
+      <div className="flex flex-col gap-6">
+        <div className="bg-[#EB966A]/25 drop-shadow-md rounded-lg w-14 h-14 flex justify-center items-center">
+          <img src={restaurant} alt="restaurant" className="h-11 w-11" />
+        </div>
+        <Link to="/">
+          <div className="group hover:bg-[#EA7C69] drop-shadow-md rounded-md w-14 h-14 flex justify-center items-center">
+            <BiHomeAlt className="h-6 w-6 text-[#EA7C69] group-hover:text-white" />
+          </div>
+        </Link>
+        <Link to="/sales-reports">
+          <div className="group hover:bg-[#EA7C69] drop-shadow-md rounded-md w-14 h-14 flex justify-center items-center">
+            <AiOutlinePieChart className="h-6 w-6 text-[#EA7C69] group-hover:text-white" />
+          </div>
+        </Link>
+        <Link to="/user-management">
+          <div className="group hover:bg-[#EA7C69] drop-shadow-md rounded-md w-14 h-14 flex justify-center items-center">
+            <FiUsers className="h-6 w-6 text-[#EA7C69] group-hover:text-white" />
+          </div>
+        </Link>
+        <Link to="/payment">
+          <div className="group hover:bg-[#EA7C69] drop-shadow-md rounded-md w-14 h-14 flex justify-center items-center">
+            <BsCart3 className="h-6 w-6 text-[#EA7C69] group-hover:text-white" />
+          </div>
+        </Link>
+        <Link to="/admin-dashboard">
+          <div className="group hover:bg-[#EA7C69] drop-shadow-md rounded-md w-14 h-14 flex justify-center items-center">
+            <MdOutlineAdminPanelSettings className="h-6 w-6 text-[#EA7C69] group-hover:text-white" />
+          </div>
+        </Link>
       </div>
-      <Link to="/">
-        <div className="group hover:bg-[#EA7C69] drop-shadow-md rounded-md w-14 h-14 flex justify-center items-center">
-          <BiHomeAlt className="h-6 w-6 text-[#EA7C69] group-hover:text-white" />
-        </div>
-      </Link>
-      <Link to="/sales-reports">
-        <div className="group hover:bg-[#EA7C69] drop-shadow-md rounded-md w-14 h-14 flex justify-center items-center">
-          <AiOutlinePieChart className="h-6 w-6 text-[#EA7C69] group-hover:text-white" />
-        </div>
-      </Link>
-      <Link to="/user-management">
-        <div className="group hover:bg-[#EA7C69] drop-shadow-md rounded-md w-14 h-14 flex justify-center items-center">
-          <FiUsers className="h-6 w-6 text-[#EA7C69] group-hover:text-white" />
-        </div>
-      </Link>
-      <Link to="/payment">
-        <div className="group hover:bg-[#EA7C69] drop-shadow-md rounded-md w-14 h-14 flex justify-center items-center">
-          <BsCart3 className="h-6 w-6 text-[#EA7C69] group-hover:text-white" />
-        </div>
-      </Link>
-      <Link to="/admin-dashboard">
-        <div className="group hover:bg-[#EA7C69] drop-shadow-md rounded-md w-14 h-14 flex justify-center items-center">
-          <MdOutlineAdminPanelSettings className="h-6 w-6 text-[#EA7C69] group-hover:text-white" />
-        </div>
-      </Link>
-      <Link to="/login">
-        <div className="group hover:bg-[#EA7C69] drop-shadow-md rounded-md w-14 h-14 flex justify-center items-center">
-          <BiLogIn className="h-6 w-6 text-[#EA7C69] group-hover:text-white" />
-        </div>
-      </Link>
+      <div>
+        <Link to="/login">
+          <div className="group hover:bg-[#EA7C69] drop-shadow-md rounded-md w-14 h-14 flex justify-center items-center">
+            <BiLogIn className="h-6 w-6 text-[#EA7C69] group-hover:text-white" />
+          </div>
+        </Link>
+      </div>
     </div>
   );
 }
