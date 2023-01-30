b5f137dd7dfe435d72364cabc464e2dc1e37b796
Krisna Sandy Pribadi <baltheon@gmail.com>
2023-01-30 16:16:19 +0700

    add search box

--- pos-app/src/pages/cashier.jsx
+++ pos-app/src/pages/cashier.jsx
@@ -1,19 +1,46 @@
+//import components
 import Card from "../components/card";
 import DisplayDate from "../components/displayDate";
+import { Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
+import { SearchIcon } from "@chakra-ui/icons";
 
 export default function Cashier() {
   return (
-    <div className="p-5 bg-[#252836] w-full">
-      <h1 className="text-white font-bold text-3xl">Jaegar Resto</h1>
-      <DisplayDate />
-      <ul className="mt-5 list-none flex flex-row flex-wrap gap-8 text-white">
-        <li>Burgers</li>
-        <li>Pizzas</li>
-        <li>Beverages</li>
-        <li>Desserts</li>
+    <div className="p-5 bg-[#252836] w-full overflow-y-auto">
+      <div className="flex flex-row justify-between">
+        <div>
+          <h1 className="text-white font-bold text-3xl mb-1">Jaegar Resto</h1>
+          <DisplayDate />
+        </div>
+        <div className="mt-2 w-80">
+          <InputGroup>
+            <InputLeftElement
+              pointerEvents="none"
+              children={<SearchIcon color="gray.300" />}
+            />
+            <Input
+              type="search"
+              placeholder="Search for food, beverages, etc..."
+              borderColor="gray"
+              className="text-white"
+            />
+          </InputGroup>
+        </div>
+      </div>
+      <ul className="mt-5 list-none flex flex-row flex-wrap gap-8 text-white mb-2">
+        <li className="hover:text-[#EA7C69]">Burgers</li>
+        <li className="hover:text-[#EA7C69]">Pizzas</li>
+        <li className="hover:text-[#EA7C69]">Desserts</li>
+        <li className="hover:text-[#EA7C69]">Beverages</li>
       </ul>
+      <hr className="border border-slate-400" />
       <h2 className="mt-5 text-white font-bold text-xl">Choose Dishes</h2>
       <div className="mt-8 bg-[#252836] w-full flex flex-row flex-wrap gap-5">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
         <Card />
         <Card />
       </div>
